797fc01b4b36c02d3b41612a692f08a4fc26ccf0
IYque <15656999965@163.com>
2025-03-13 16:49:36 +0800

    ====

--- icode-ui/src/stores/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  sidebar: (state) => state.app.sidebar,
-  avatar: (state) => state.user.avatar,
-  name: (state) => state.user.name,
-  sidebarRouters: (state) => state.app.sidebarRouters,
-}
-export default getters
