567ef1a6d53c13da869c0ee4d49e1ad7b62b14e4
lefulana <14511225+lefulana@user.noreply.gitee.com>
2025-04-06 00:42:41 +0800

    sdk

--- frontEnd/pc/src/main.ts
+++ frontEnd/pc/src/main.ts
@@ -40,43 +40,54 @@ for (const c in components) {
 }
 
 // 全局方法挂载
+import { SDK, $sdk } from './utils/sdk'
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $sdk: SDK
+  }
+}
 import * as methods from '@/utils/common'
-Object.assign(app.config.globalProperties, methods, {
-  // 动态配置
-  sysConfig: window.sysConfig,
-  // 日期时间控件快捷配置
-  msgSuccess(msg = '操作成功') {
-    this.$message({
-      showClose: true,
-      message: msg,
-      type: 'success',
-    })
+Object.assign(
+  app.config.globalProperties,
+  methods,
+  { $sdk },
+  {
+    // 动态配置
+    sysConfig: window.sysConfig,
+    // 日期时间控件快捷配置
+    msgSuccess(msg = '操作成功') {
+      this.$message({
+        showClose: true,
+        message: msg,
+        type: 'success',
+      })
+    },
+    msgError(msg = '操作失败') {
+      this.$message({ showClose: true, message: msg, type: 'error' })
+    },
+    msgInfo(msg) {
+      this.$message.info(msg)
+    },
+    $confirm(message = '确认删除吗？', title, options = {}) {
+      let defaultConfig = {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        title: '警告',
+      }
+      if (typeof title === 'string') {
+        defaultConfig.title = title
+        Object.assign(options, defaultConfig)
+      } else {
+        Object.assign(options, defaultConfig, title || {})
+      }
+      return ElMessageBox.confirm(message, title, options)
+    },
+    // $confirmDel(message = '确认删除吗？', title, options = {}) {
+    //   this.$confirm(message, title, options)
+    // },
   },
-  msgError(msg = '操作失败') {
-    this.$message({ showClose: true, message: msg, type: 'error' })
-  },
-  msgInfo(msg) {
-    this.$message.info(msg)
-  },
-  $confirm(message = '确认删除吗？', title, options = {}) {
-    let defaultConfig = {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning',
-      title: '警告',
-    }
-    if (typeof title === 'string') {
-      defaultConfig.title = title
-      Object.assign(options, defaultConfig)
-    } else {
-      Object.assign(options, defaultConfig, title || {})
-    }
-    return ElMessageBox.confirm(message, title, options)
-  },
-  // $confirmDel(message = '确认删除吗？', title, options = {}) {
-  //   this.$confirm(message, title, options)
-  // },
-})
+)
 
 import stores, { store } from '@/stores'
 
