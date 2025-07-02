f2de23191f91814990f99812bd3bd7457302bab7
IYque <15656999965@163.com>
2025-07-02 14:16:40 +0800

    组合话术与群机器人接口提交

--- src/main/java/cn/iyque/dao/IYqueCategoryDao.java
+++ src/main/java/cn/iyque/dao/IYqueCategoryDao.java
@@ -1,8 +1,13 @@
 package cn.iyque.dao;
 
 import cn.iyque.entity.IYqueCategory;
+import jdk.internal.net.http.common.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface IYqueCategoryDao extends JpaRepository<IYqueCategory,Long>, JpaSpecificationExecutor<IYqueCategory> {
+
+
 }
--- src/main/java/cn/iyque/enums/MediaType.java
+++ src/main/java/cn/iyque/enums/MediaType.java
@@ -9,7 +9,8 @@ public enum MediaType {
     A("0","图片"),
     B("4","文本"),
     C("7","热词分类"),
-    D("9","图文");
+    D("9","图文"),
+    E("13","组合话术");
 
 
     private String key;
