c924fdf792083b64e6fc6b85391fafb200284410
qq_24315921 <qq_24315921@noreply.gitcode.com>
2025-07-23 22:54:38 +0800

    update: 更新文件 CORSConfig.java
    Signed-off-by: qq_24315921 <qq_24315921@noreply.gitcode.com>

--- src/main/java/cn/iyque/config/CORSConfig.java
+++ src/main/java/cn/iyque/config/CORSConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 跨域相关配置
+ */
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
 
