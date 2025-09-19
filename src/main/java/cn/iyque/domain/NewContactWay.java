a7dcbf9a061c21733d8d2d78283eee1d1b84c4a2
IYque <15656999965@163.com>
2025-09-19 11:11:02 +0800

    相关问题修复

--- src/main/java/cn/iyque/domain/NewContactWay.java
+++ src/main/java/cn/iyque/domain/NewContactWay.java
@@ -1,6 +1,5 @@
 package cn.iyque.domain;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import me.chanjar.weixin.cp.bean.external.WxCpContactWayInfo;
@@ -8,6 +7,5 @@ import me.chanjar.weixin.cp.bean.external.WxCpContactWayInfo;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class NewContactWay extends WxCpContactWayInfo.ContactWay{
-    @SerializedName("is_exclusive")
-    private Boolean isExclusive;
+
 }
